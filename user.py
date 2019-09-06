4c304f90b8876c4cf0613b318dfdcc1502fefb8a
Bey Faith <fryumugabe@gmail.com>
2019-09-06 15:54:52 +0200

    add arguments to my user class.

--- user.py
+++ user.py
@@ -1,7 +1,16 @@
 class User:
-    
+
     """
 
     Class that generates new instances of user login into my aplication.
 
     """
+
+    def __init__(self,first_name,last_name,user_name,password):
+
+        """
+        Define properties for my object.
+
+        """
+
+        
