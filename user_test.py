0c77620aac496dd94903c8011ebeb8d900f945a9
Bey Faith <fryumugabe@gmail.com>
2019-09-08 01:07:22 +0200

    add first test to my test class.

--- user_test.py
+++ user_test.py
@@ -11,4 +11,19 @@ class TestUser(unittest.TestCase):
     '''
     def setUp(self):
 
-        self.new_user = User("Bey","Faith","fryumugabe@gmail.com","hello123")
+        self.new_user = User("Bey","Faith","fryumugabe@gmail.com","hello")
+
+    def test_1(self):
+
+        '''
+        test case  to test the initilization of the object.
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Bey")
+        self.assertEqual(self.new_user.last_name,"Faith")
+        self.assertEqual(self.new_user.email,"fryumugabe@gmail.com")
+        self.assertEqual(self.new_user.password,"hello")
+
+
+if__name__ == '__main__':
+    unittest.main()        
