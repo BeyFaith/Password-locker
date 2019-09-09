85d487c7ac4a6db2ca7c3731540a0398edcdf966
Bey Faith <fryumugabe@gmail.com>
2019-09-09 10:34:19 +0200

    createfirst test for my credentials class.

--- credentials_test.py
+++ credentials_test.py
@@ -7,4 +7,14 @@ class TestInfo(unittest.TestCase):
     test class that will define tests for credentials class.
 
     '''
-    
+    def setUp(self):
+        self.new_info= Info("bey.faith","faith.bey")
+
+    def test_init(self):
+        self.assertEqual(self.new_info.face_bookp,"bey.faith")
+        self.assertEqual(self.new_info.email_p,"faith.bey")
+
+
+
+if __name__ == '__main__':
+    unittest.main()     
