be820f525c4982947b54859cb729b7bfb5182d7e
Bey Faith <fryumugabe@gmail.com>
2019-09-09 10:24:47 +0200

    create test class for credentials.

--- credentials.py
+++ credentials.py
@@ -1,8 +1,25 @@
-classinfo:
+class Info:
 
-'''
-class that generates new instances of login credentials.
+    '''
+    class that generates new instances of login credentials.
 
-'''
+    '''
 
-info_list = []
+    info_list = []
+
+    def __init__(self,face_bookp,email_book_p):
+
+        '''
+        define properties for my class.
+
+        '''
+        self.face_bookp = face_bookp
+        self.email_p = email_book_p
+
+    def  save_info(self):
+
+        '''
+        function to save credentials
+
+        '''
+        Info .info_list.append(self)
---
+++ credentials_test.py
@@ -0,0 +1,10 @@
+import unittest
+from credentials import Info
+
+class TestInfo(unittest.TestCase):
+
+    '''
+    test class that will define tests for credentials class.
+
+    '''
+    
