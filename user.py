73c0da2bb3530e92b5d8a771ed58982af25e98bf
Bey Faith <fryumugabe@gmail.com>
2019-09-09 22:01:13 +0200

    add some credential methods to my app.

--- user.py
+++ user.py
@@ -30,5 +30,54 @@ class User:
         User.users_list.append(self)
 
 class Credential:
-    
-       
+
+    '''
+    class that generates new instances of login credentials.
+
+    '''
+
+    credentials_list = []
+
+    user_credentials_list = []
+
+    @classmethod
+    def check_user(cls,first_name,password):
+        
+        '''
+        method to checkif user and password entered matches what is entered.
+        '''
+        current_user = ''
+        for user in User.users_list:
+            if(user.first_name ==first_name and user.password == password):
+                current_user = user.first_name
+            return current_user
+
+    def __init__(self,user_name,site_name,account_name,password):
+
+        '''
+        method to definetheproperties for user object.
+        '''
+
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.password = password
+
+    def save_credentials(self):
+
+        '''
+        function to save new created credentials.
+        '''
+
+        Credential.credentials_list.append(self)
+
+    def generate_password(size=5, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+
+        '''
+        function that willgenerate a 5 character password.
+        '''
+        gen_pass = ''.join(random.choice(char) for x inrange(size))
+        return gen_pass
+
+    @classmethod
+    defd    
