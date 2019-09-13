49ae64f673b8c39cef2aa103da82d52adda48532
Bey Faith <fryumugabe@gmail.com>
2019-09-13 12:44:43 +0200

    add a test user class to my project.

--- run.py
+++ run.py
@@ -1,142 +1,155 @@
-#!/usr/bin/env python3.6
+#! /usr/bin/env python3
+import pyperclip
+from userCredential import User, Credential
 
-from user import User
-from credentials import Info
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname,lname,password)
+	return new_user
 
-def create_account(first_name,last_name,email):
-    '''
-    function to create new account user
-    '''
+def save_user(user):
+	'''
+	Function to save a new user account
+	'''
+	User.save_user(user)
 
-    new_user = user(first_name,last_name,email)
-    return new_user
 
-def create_credentials(facebookp,emailp):
-    '''
-    function to create new credential.
-    '''
+def verify_user(first_name,password):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	checking_user = Credential.check_user(first_name,password)
+	return checking_user
 
-    new_cred = Info(facebookp,emailp)
-    return new_cred
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
 
-def save_account(user):
-    
-    '''
-    function to save user
-    '''
+def create_credential(user_name,site_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,site_name,account_name,password)
+	return new_credential
 
-    user.save_user()
+def save_credential(credential):
+	'''
+	Function to save a newly created credential
+	'''
+	Credential.save_credentials(credential)
 
-def save_credentials(credentials):
-
-    '''
-    function to save credentials.
-    '''
-
-    credentials.save_info()
-
-def display_users():
-    '''
-    function to display users
-    '''
-    return User.display_users()
-
-def  display_creds():
-    '''
-    function to display credentials
-    '''
-    return Info.display_info()  
-
+def display_credentials(user_name):
+	'''
+	Function to display credentials saved by a user
+	'''
+	return Credential.display_credentials(user_name)
+	
+def copy_credential(site_name):
+	'''
+	Function to copy a credentials details to the clipboard
+	'''
+	return Credential.copy_credential(site_name)
 
 def main():
-    print("Hello welcome to your password-locker app.")
-    #print('/n')    
-    
-    while True:
-        print("-" * 156)
-        print(''' Use the following short codes: cc-create new account, ex-exit password locker,dac-display accounts,gs-generate passwords''' )
-        print(" ")
-        print("  Type in a short code!")
-        print(" ")
+	print(' ')
+	print('Hello! Welcome to Password Locker.')
+	while True:
+		print(' ')
+		print("-"*60)
+		print('Use these codes to navigate: \n ca-Create an Account \n li-Log In \n ex-Exit')
+		short_code = input('Enter a choice: ').lower().strip()
+		if short_code == 'ex':
+			break
 
-        short_code = input().lower()
-        if short_code =='cc':
-            print(" ")
-            print("-" * 156)
-            print("      Createa new account")
-            print(" ")
-            print(" ")
-            print("what is your first name?")
-            print(" ")
-            first_name = input()
-            print("What is your last name?")
-            print(" ")
-            last_name = input()
-            print("what is your email address?")
-            print(" ")
-            email = input()
-            print("what is your facebook password?")
-            print(" ")
-            face_bookp = input()
-            print("what is your email password?")
-            print(" ")
-            emailp = input()
-            save_account(create_account(first_name,last_name,email))
-            print('/n')
-            save_credentials(create_credentials(face_bookp,emailp))
-            print('\n')
-            print("-" *156)
-            print(f"New Account {first_name} {last_name} {face_bookp} has been created")
-            print('\n')
-        
-        elif short_code == 'dac':
+		elif short_code == 'ca':
+			print("-"*60)
+			print(' ')
+			print('To create a new account:')
+			first_name = input('Enter your first name - ').strip()
+			last_name = input('Enter your last name - ').strip()
+			password = input('Enter your password - ').strip()
+			save_user(create_user(first_name,last_name,password))
+			print(" ")
+			print(f'New Account Created for: {first_name} {last_name} using password: {password}')
+		elif short_code == 'li':
+			print("-"*60)
+			print(' ')
+			print('To login, enter your account details:')
+			user_name = input('Enter your first name - ').strip()
+			password = str(input('Enter your password - '))
+			user_exists = verify_user(user_name,password)
+			if user_exists == user_name:
+				print(" ")
+				print(f'Welcome {user_name}. Please choose an option to continue.')
+				print(' ')
+				while True:
+					print("-"*60)
+					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+					short_code = input('Enter a choice: ').lower().strip()
+					print("-"*60)
+					if short_code == 'ex':
+						print(" ")
+						print(f'Goodbye {user_name}')
+						break
+					elif short_code == 'cc':
+						print(' ')
+						print('Enter your credential details:')
+						site_name = input('Enter the site\'s name- ').strip()
+						account_name = input('Enter your account\'s name - ').strip()
+						while True:
+							print(' ')
+							print("-"*60)
+							print('Choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+							psw_choice = input('Enter an option: ').lower().strip()
+							print("-"*60)
+							if psw_choice == 'ep':
+								print(" ")
+								password = input('Enter your password: ').strip()
+								break
+							elif psw_choice == 'gp':
+								password = generate_password()
+								break
+							elif psw_choice == 'ex':
+								break
+							else:
+								print('Try again.')
+						save_credential(create_credential(user_name,site_name,account_name,password))
+						print(' ')
+						print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+						print(' ')
+					elif short_code == 'dc':
+						print(' ')
+						if display_credentials(user_name):
+							print('Here is a list of all your credentials')
+							print(' ')
+							for credential in display_credentials(user_name):
+								print(f'Site Name: {credential.site_name} - Account Name: {credential.account_name} - Password: {credential.password}')
+							print(' ')	
+						else:
+							print(' ')
+							print("You don't have any credentials saved yet")
+							print(' ')
+					elif short_code == 'copy':
+						print(' ')
+						chosen_site = input('Enter the site name for the credential password to copy: ')
+						copy_credential(chosen_site)
+						print('')
+					else:
+						print('Try again.')
 
-            if display_users():
-                print(" ")
-                print("The user name")
-                print(" ")
-                print('\n')
-                for user in display_users():
-                    print(f"{user.first_name} {user.last_name}")
-                for credentials in display_creds():
-                    print(f"{face_bookp}")
-                    print(" ")
-
-            else:
-                print('\n')    
-                print("-" * 156)
-                print(" ")
-                print("  Please create an account")
-                print("   you have not created an account with password-locker yet!!")
-                print(" ")
-
-
-        elif short_code == 'gs':
-            print(" ")
-            print(" ")
-            print("To generate password add in your first name and facebook below.")
-            print(" ")
-            list_of_inputs = [c for c in input()]
-            list_of_inputs.reverse()
-            print(list_of_inputs)
-
-
-        elif short-code == "ex":
-            print("-" * 156)
-            print(" ")
-            print("Thanks for usingpassword-locker!")
-            print("Bye!!!")
-            print(" ")
-            print("-" * 156)
-            break
-
-        else:
-            print("-" * 156)
-            print(" ")
-            print("  retry!")
-            print(" ")
-            print("   Please select one of the available options.")
-            print(" ")
+			else: 
+				print(' ')
+				print('Try again or Create an Account.')		
+		
+		else:
+			print("-"*60)
+			print(' ')
+			print('Try again.')				
 
 if __name__ == '__main__':
-    main()           
+	main()
