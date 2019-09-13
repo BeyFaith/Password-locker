48e0a890025f80fb03e65d6d8395ef6b56690872
Bey Faith <fryumugabe@gmail.com>
2019-09-13 12:55:06 +0200

    create the init function that defines properties.

--- userCredential.py
+++ userCredential.py
@@ -45,10 +45,7 @@ class Credential:
 		Method to define the properties for each user object will hold.
 		'''
 
-		self.user_name = user_name
-		self.site_name = site_name
-		self.account_name = account_name
-		self.password = password
+		
 
 	def save_credentials(self):
 		'''
