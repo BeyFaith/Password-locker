60f1734ea523f2e23a55015c87a900028fe92f60
Bey Faith <fryumugabe@gmail.com>
2019-09-13 12:55:35 +0200

    define my properties.

--- userCredential.py
+++ userCredential.py
@@ -45,7 +45,10 @@ class Credential:
 		Method to define the properties for each user object will hold.
 		'''
 
-		
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
 
 	def save_credentials(self):
 		'''
